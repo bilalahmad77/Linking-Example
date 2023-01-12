5cdf17ce0d7267009b2adcfd334f6b3521c5694d
bilalahmad77 <mebilalahmad1@gmail.com>
2023-01-12 18:41:47 +0500

    Linking-example
    linking-example

---
+++ linking-example/app/name/address/page.tsx
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+export default function GiveAddress() {
+    return (
+      <div>
+            I live in Rawalpindi, Pakistan
+            <br/>
+            <Link href="/">Go to home/index page</Link>
+      </div>
+    )
+  }
